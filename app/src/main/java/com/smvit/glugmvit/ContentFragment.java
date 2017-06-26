718cae049c0681ade8145b3cbff6f866cf862b14
Susmit-A <susmit600@gmail.com>
2017-06-26 11:37:39 +0530

    Basic skeleton UI by Susmit on 26/06/2017 11:37 AM.

--- app/src/main/java/com/smvit/glugmvit/ContentFragment.java
+++ app/src/main/java/com/smvit/glugmvit/ContentFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * Created by susmit on 24/6/17.
