1143e7fc39e26881c92a9771652c81c8ed91900d
vgaurav3011 <vgaurav3011@gmail.com>
2017-08-17 21:45:49 +0530

    Added Splash Screen Added loginpage xml document Please work on accessibility part of login

--- app/src/main/java/com/smvit/glugmvit/SplashScreen.java
+++ app/src/main/java/com/smvit/glugmvit/SplashScreen.java
@@ -2,24 +2,29 @@ package com.smvit.glugmvit;
 
 
         import android.content.Intent;
+        import android.os.AsyncTask;
         import android.os.Bundle;
         import android.support.design.widget.FloatingActionButton;
         import android.support.design.widget.Snackbar;
         import android.support.v7.app.AppCompatActivity;
         import android.support.v7.widget.Toolbar;
         import android.view.View;
+        import android.widget.Button;
 
 public class SplashScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         Thread myThread=new Thread(){
             @Override
             public void run() {
                 try {
                     sleep(3000);
+
                     Intent intent=new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
@@ -29,11 +34,11 @@ public class SplashScreen extends AppCompatActivity {
 
             }
 
-
-
-
         };myThread.start();
 
     }
 
+
 }
+
+
